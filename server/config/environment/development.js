38e51db7e9840a5369bb7adcd71766b197bf1515
moltak <moltak@gmail.com>
2014-12-25 14:39:40 +0900

    Initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/randomtable-dev'
+  },
+
+  seedDB: true
+};
